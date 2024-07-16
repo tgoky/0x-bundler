32e30b1d91895ef3ada4bf393583a937772d4aae
tgoky <tgokyicloud6@gmail.com>
2024-07-16 13:27:54 +0100

    f

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
 };
 
 export default config;
